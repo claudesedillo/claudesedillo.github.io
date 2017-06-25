b69ef53c4b8d0446301db4515087d4967d1b3838
AlexAgnoII <3alex23@gmail.com>
2017-06-25 19:10:18 +0800

    Added Information and Posts tabs functionality and values in profile

--- profileJQ.js
+++ profileJQ.js
@@ -1,7 +1,10 @@
-var photos, albums, posts, users;
-var getPhotos, getAlbums, getPosts, getUsers;
-var userName;
+var photos, albums, posts, users; //collection of all
+var getPhotos, getAlbums, getPosts, getUsers; //functions
 
+var theUser;
+var ownAlbum = [0], ownPosts = [0], ownPhotos = [0]; //specifically for that certain user.
+var albumIndex = 0, ownPostsIndex = 0, ownPhotosIndex = 0;
+var temp = (window.location.href).split("=");
 
 //This variable is a count to determine the posts to be loaded
 //Starting for the recent post (index 9);
@@ -63,29 +66,112 @@ getPosts = new Promise(function(resolve, reject) {
 //This function returns the name of the user, given the ID.
 function searchUser(theId) {
     var x = 0;
+    var found = false
     
-    while(x < users.length) {
+    while(x < users.length &&!found) {
         
         if(users[x].id == theId) {
-            return users[x].username;
+            theUser = users[x];
+            found = true;
         }
         
         x++;
     }
-    
-    return "No name";
+    if(found == false)
+        theUser = null;
     
 }
 
 function setUserTab() {
-    var temp = (window.location.href).split("=");
-    userName = searchUser(temp[1]);
+   //var temp = (window.location.href).split("=");
+    
+    searchUser(temp[1]);
         
-    console.log("UserID: " + temp[1] + " Username: " + userName);
-    $("span#userName").html(userName);
+    console.log("UserID: " + temp[1] + " Username: " + theUser.username);
+    $("span.userName").html(theUser.username);
+    $("#tabSwipe").tabs({ 'swipeable': true });
 }
 
+function fillupData() {
+    //Fill up personal information
+    $("span#name").html(theUser.name);
+    $("span#email").html(theUser.email);
+    $("span#phone").html(theUser.phone);
+    $("span#website").html(theUser.website);
+    
 
+    //fill up address
+    $("span#street").html(theUser.address.street);
+    $("span#suite").html(theUser.address.suite);
+    $("span#city").html(theUser.address.city);
+    $("span#zipcode").html(theUser.address.zipcode);
+    
+    //fill up company
+    $("span#compName").html(theUser.company.name);
+    $("span#phrase").html(theUser.company.catchPhrase);
+    $("span#business").html(theUser.company.bs);
+}
+
+function getMyPost() {
+    var theID = temp[1];
+    var sum = 0;
+    var i;
+    console.log("inside postCount: " + theID);
+    
+    for(i = 0; i < posts.length; i++) {
+        if(theID == posts[i].userId) {
+            ownPosts[ownPostsIndex] = posts[i];
+            ownPostsIndex++;
+        }
+    }
+    
+    console.log("Total posts: " + ownPosts.length);
+    return ownPosts.length;
+    
+}
+
+function createPosts() {
+    var aUl = $("<ul></ul>");
+    var aLi;
+    var divHead;
+    var divBody;
+    var i; //index;
+    var tempIndex;
+    var size;
+
+    size = getMyPost();
+    tempIndex = size - 1;
+    aUl.attr("class", "collapsible popout");
+    
+    
+    
+    //For loop
+    for(i = 0; i < size; i++) {
+        
+        //get title
+        divHead = $("<div></div>");
+        divHead.attr("class", "collapsible-header");
+        divHead.html(ownPosts[tempIndex - i].title);
+    
+        //get body
+        divBody = $("<div></div>");
+        divBody.attr("class", "collapsible-body");
+        divBody.html(ownPosts[tempIndex - i].body);
+        
+        //append to li
+        aLi = $("<li></li>");
+        aLi.append(divHead);
+        aLi.append(divBody);
+        
+        //append to ul
+        aUl.append(aLi);
+    }
+    
+    
+    $("div#posts").append(aUl);
+    $('.collapsible').collapsible(); 
+    //Do not place this before the append. It will not work.
+}
 
 $("document").ready(function() {
     
@@ -105,7 +191,9 @@ $("document").ready(function() {
         
 
         setUserTab();
-        $("#tabSwipe").tabs({ 'swipeable': true });
+        fillupData();
+        createPosts();
+       
     });
     
     
